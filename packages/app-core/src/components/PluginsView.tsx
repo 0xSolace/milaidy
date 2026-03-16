9fd5b093eaedafdd835cc6564c98388ac18e7281
Shaw <shawmakesmagic@gmail.com>
2026-03-15 19:48:27 -0700

    milady: merge connector fixes into extracted worktree

--- packages/app-core/src/components/PluginsView.tsx
+++ packages/app-core/src/components/PluginsView.tsx
@@ -1987,7 +1987,12 @@ function PluginListView({ label, mode = "all", inModal }: PluginListViewProps) {
                 </div>
               </div>
               <nav className="flex-1 space-y-2 overflow-y-auto px-4 py-4">
-                {visiblePlugins.map((plugin) => {
+                {(desktopConnectorLayout
+                  ? visiblePlugins.filter((plugin) => {
+                      return plugin.id === connectorSelectedId;
+                    })
+                  : visiblePlugins
+                ).map((plugin) => {
                   const isSelected = connectorSelectedId === plugin.id;
                   const isExpanded = connectorExpandedIds.has(plugin.id);
                   const isToggleBusy = togglingPlugins.has(plugin.id);
