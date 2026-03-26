1a3d403e9176ff10e24e87f71526ff5931919140
dutch <dutchiono@gmail.com>
2026-03-26 09:04:07 -0400

    chore: restore typecheck dependencies and vrm type shims

---
+++ packages/app-core/src/types/three-vrm-shim.d.ts
@@ -0,0 +1,11 @@
+declare module "@pixiv/three-vrm" {
+  export const MToonMaterialLoaderPlugin: any;
+  export type VRMHumanBoneName = string;
+  export type VRM = any;
+  export const VRMLoaderPlugin: any;
+  export const VRMUtils: any;
+}
+
+declare module "@pixiv/three-vrm/nodes" {
+  export const MToonNodeMaterial: any;
+}
